36235b1fa81f42e6e20fbf8ee73d807758c61442
DingYi <dvdface@users.noreply.github.com>
2025-04-04 19:47:07 +0800

    Update run_task_evolution.bat

--- Mobile-Agent-E/scripts/run_task_evolution.bat
+++ Mobile-Agent-E/scripts/run_task_evolution.bat
@@ -1,6 +1,3 @@
 set TASK_JSON_PATH=data/custom_tasks_example.json
 set SETTING=evolution
-python run.py ^
-    --run_name "example-%SETTING%" ^
-    --setting %SETTING% ^
-    --tasks_json "%TASK_JSON_PATH%" 
+python run.py --run_name "example-%SETTING%" --setting %SETTING% --tasks_json "%TASK_JSON_PATH%" 
