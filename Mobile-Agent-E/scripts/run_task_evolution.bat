a63327484621336213e2606809a85274802d6dfc
DingYi <dvdface@users.noreply.github.com>
2025-04-04 18:24:10 +0800

    Create run_task_evolution.bat for running under windows

---
+++ Mobile-Agent-E/scripts/run_task_evolution.bat
@@ -0,0 +1,6 @@
+set TASK_JSON_PATH="data/custom_tasks_example.json"
+set SETTING="evolution"
+python run.py ^
+    --run_name "example-%SETTING%" ^
+    --setting %SETTING% ^
+    --tasks_json "%TASK_JSON_PATH%" 
